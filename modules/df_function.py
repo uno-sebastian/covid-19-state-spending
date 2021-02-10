f073ca1f3f5c5b8765a210beaedc71673e6f161a
Sebastian Echeverry <uno.sebastian.echo@gmail.com>
2021-02-09 23:45:42 -0500

    cleanup and getting ready for merge

---
+++ modules/df_function.py
@@ -0,0 +1,37 @@
+# to handle  data retrieval
+import requests
+# to handle certificate verification
+import certifi
+# to manage json data
+import json
+# for pandas dataframes
+import pandas as pd
+
+__author__ = 'Mark Chua'
+__credits__ = ['Mark Chua']
+__version__ = '1.0.0'
+__maintainer__ = 'Mark Chua'
+__email__ = 'mchua004@gmail.com'
+__status__ = 'Development'
+
+output_file = 'covid_data/data.csv'
+
+# return the covid data as a pandas dataframe
+def get_covid_data():
+    # URL for GET requests to retrieve vehicle data
+    url = 'https://covidtracking.com/api/states/daily'
+
+    # Perform a get request for this character
+    response = requests.get(url)
+
+    # Storing the JSON response within a variable
+    data = response.json()
+
+    # in this dataset, the data to extract is under 'features'
+    covidtracking_df = pd.json_normalize(data)
+
+    # export dataframe to a csv file
+    covidtracking_df.to_csv(output_file, index = False, header=True)
+
+    # send off the data to whomever called me!
+    return covidtracking_df
