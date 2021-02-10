95dfe02ccedc0003e24ff2ea4ebc3a436ca27123
Sebastian Echeverry <uno.sebastian.echo@gmail.com>
2021-02-09 21:04:01 -0500

    fix

--- modules/crfb.py
+++ modules/crfb.py
@@ -7,6 +7,8 @@ import pandas as pd
 out = widgets.Output(layout={'border': '1px solid black'})
 
 crfb_files = {
+    '2021-02-09' : '20210209_cmt.xlsx',
+    '2021-02-08' : '20210208_alt_cmt.xlsx',
     '2021-02-03' : '20210203_cmt.csv',
     '2021-02-02' : '20210202_new_alternative_cmt.xlsx',
     '2021-01-25' : '20210125_cmt.xlsx',
@@ -62,6 +64,7 @@ valid_states = [
     'Oklahoma',
     'Oregon',
     'Pennsylvania',
+    'Pennsylvannia',
     'Rhode Island',
     'South Carolina',
     'South Dakota',
@@ -76,6 +79,15 @@ valid_states = [
     'Wyoming',
 ]
 
+crfb_headers = [
+    'Recipient State',
+    'Amount Committed/Disbursed',
+    'Date',
+    'Recipient Type',
+    'Legislation',
+    'Agency'
+]
+
 # get the multi values indexes in the serise column
 def get_multi_values_indexs(column):
     mulit_values = []
@@ -87,7 +99,7 @@ def get_multi_values_indexs(column):
 def split_mulit_states(compressed_data, exclude_states):
     uncompressed = pd.DataFrame()
     for index, row in compressed_data.iterrows():
-        split_names = str(row['Recipient State']).split(';')
+        split_names = fr'{row["Recipient State"]}'.split(';')
         row['Amount Committed/Disbursed'] = row['Amount Committed/Disbursed'] / len(split_names)
         for state_name in split_names:
             if exclude_states and state_name not in valid_states:
@@ -96,15 +108,6 @@ def split_mulit_states(compressed_data, exclude_states):
             new_row['Recipient State'] = state_name
             uncompressed = pd.concat([uncompressed, new_row])
     return uncompressed
-
-crfb_headers = [
-    'Recipient State',
-    'Amount Committed/Disbursed',
-    'Date',
-    'Recipient Type',
-    'Legislation',
-    'Agency'
-]
 
 def clean_raw_crfb_data(raw_data, exclude_states):
     """
@@ -131,11 +134,11 @@ def clean_raw_crfb_data(raw_data, exclude_states):
         states = split_mulit_states(pd.DataFrame(data.iloc[mulit_states,:]), exclude_states)
         data.drop(mulit_states, inplace=True)
         data.reset_index(drop=True, inplace=True)
-        if states.size > 0:
+        if states.shape[0] > 0:
             data = pd.concat([data, states])
     return data.reset_index(drop=True)
 
-def get_data(exclude_states=True):
+def get_data_date(date, exclude_states=True):
     """
     Get the data from the Committee for a Responsible Federal Budget 
     
@@ -146,24 +149,51 @@ def get_data(exclude_states=True):
     data_path = os.path.dirname(os.path.realpath(__file__))
     data_path = os.path.join(data_path, 'crfb_raw_data')
     # create temp var to hold our data
+    df = pd.DataFrame()
+    if date not in crfb_files:
+        print(f'there is no file for date {date}')
+        return df
+    print(f'loading data from date {date} : file {crfb_files[date]}...')
+    path = os.path.join(data_path, crfb_files[date])
+    try:
+        df = pd.DataFrame()
+        if path.endswith('.csv'): df = pd.read_csv(path)
+        else: df = pd.read_excel(path, index_col=0)
+        df = clean_raw_crfb_data(df, exclude_states)
+    except Exception as e:
+        print(f'failed to load data {path}\nERROR: {e}')
+    return df
+
+def get_data_single(index=0, exclude_states=True):
+    """
+    Get the data from the Committee for a Responsible Federal Budget 
+    
+    Parameters
+    ----------
+        exclude_states — Exclude any state outside the common 50 known
+    """
+    date = [key for key in crfb_files.keys()][index]
+    return get_data_date(date, exclude_states)
+
+def get_data_all(exclude_states=True):
+    """
+    Get the data from the Committee for a Responsible Federal Budget 
+    
+    Parameters
+    ----------
+        exclude_states — Exclude any state outside the common 50 known
+    """
+    # create temp var to hold our data
     crfb_data = pd.DataFrame()
     for key, value in crfb_files.items():
-        print(f'loading data from date {key} : file {value}...')
-        path = os.path.join(data_path, value)
-        try:
-            df = pd.DataFrame()
-            if path.endswith('.csv'): df = pd.read_csv(path)
-            else: df = pd.read_excel(path, index_col=0)
-            df = clean_raw_crfb_data(df, exclude_states)
-            if df.size > 0:
-                re_order = ['Date Uploaded'] + list(df.columns)
-                write_date = pd.Series([key for i in range(len(df['Amount Committed/Disbursed']))])
-                df[re_order[0]] = pd.to_datetime(write_date, format='%Y-%m-%d')
-                crfb_data = pd.concat([crfb_data, df.reindex(columns=re_order)])
-            else:
-                parsed = json.loads(df.to_json())
-                print(f'failed to concat dataframe of value\n{json.dumps(parsed, indent=4) }')
-        except:
-            print(f'failed to load data {path}')
+        df = get_data_date(key, exclude_states)
+        if df.shape[0] > 0:
+            re_order = ['Date CRFB Downloaded'] + list(df.columns)
+            write_date = pd.Series([key for i in range(len(df['Amount Committed/Disbursed']))])
+            df[re_order[0]] = pd.to_datetime(write_date, format='%Y-%m-%d')
+            crfb_data = pd.concat([crfb_data, df.reindex(columns=re_order)])
+        else:
+            parsed = json.loads(df.to_json())
+            print(f'failed to concat dataframe of value\n{json.dumps(parsed, indent=4) }')
     out.clear_output()
     return crfb_data
